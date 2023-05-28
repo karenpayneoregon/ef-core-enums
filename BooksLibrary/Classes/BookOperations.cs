fe493542a75a0bf5cbdb511a4da81d1d0b580827
Karen Payne <paynekaren@comcast.net>
2023-05-28 15:17:58 -0700

    minor refactor

--- BooksLibrary/Classes/BookOperations.cs
+++ BooksLibrary/Classes/BookOperations.cs
@@ -13,121 +13,113 @@ public class BookOperations
     public static List<Book> AddViewBooks(bool createDatabase = false)
     {
         using var context = new BookContext();
-            
-        if (createDatabase)
+
+        if (!createDatabase)
+            return context
+                .Books
+                .Include(item => item.BookConnect)
+                .ToList();
+
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+
+        context.Books.Add(new Book
         {
+            Title = "Sports Illustrated",
+            BookConnectId = BookConnectId.Sports
+        });
 
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
+        context.Books.Add(new Book
+        {
+            Title = "Friday Night Lights",
+            BookConnectId = BookConnectId.Sports
+        });
 
-            context.Books.Add(new Book
-            {
-                Title = "Sports Illustrated",
-                BookConnectId = BookConnectId.Sports
-            });
+        context.Books.Add(new Book
+        {
+            Title = "The logic of sports betting",
+            BookConnectId = BookConnectId.Sports
+        });
 
-            context.Books.Add(new Book
-            {
-                Title = "Friday Night Lights",
-                BookConnectId = BookConnectId.Sports
-            });
+        context.Books.Add(new Book
+        {
+            Title = "Git for programmers",
+            BookConnectId = BookConnectId.Programming
+        });
 
-            context.Books.Add(new Book
-            {
-                Title = "The logic of sports betting",
-                BookConnectId = BookConnectId.Sports
-            });
+        context.Books.Add(new Book
+        {
+            Title = "The self-taught programmer",
+            BookConnectId = BookConnectId.Programming
+        });
 
-            context.Books.Add(new Book
-            {
-                Title = "Git for programmers",
-                BookConnectId = BookConnectId.Programming
-            });
+        context.Books.Add(new Book
+        {
+            Title = "C# Programming",
+            BookConnectId = BookConnectId.Programming
+        });
 
-            context.Books.Add(new Book
-            {
-                Title = "The self-taught programmer",
-                BookConnectId = BookConnectId.Programming
-            });
+        context.Books.Add(new Book
+        {
+            Title = "Beginning Object-Oriented Programming with C#",
+            BookConnectId = BookConnectId.Programming
+        });
 
-            context.Books.Add(new Book
-            {
-                Title = "C# Programming",
-                BookConnectId = BookConnectId.Programming
-            });
-
-            context.Books.Add(new Book
-            {
-                Title = "Beginning Object-Oriented Programming with C#",
-                BookConnectId = BookConnectId.Programming
-            });
-
-            context.Books.Add(new Book
-            {
-                Title = "Entity Framework Core in Action",
-                BookConnectId = BookConnectId.Programming
-            });
+        context.Books.Add(new Book
+        {
+            Title = "Entity Framework Core in Action",
+            BookConnectId = BookConnectId.Programming
+        });
 
 
-            context.Books.Add(new Book
-            {
-                Title = "Into the Abyss: An Extraordinary True Story",
-                BookConnectId = BookConnectId.Adventure
-            });
+        context.Books.Add(new Book
+        {
+            Title = "Into the Abyss: An Extraordinary True Story",
+            BookConnectId = BookConnectId.Adventure
+        });
 
-            context.Books.Add(new Book
-            {
-                Title = "The Terminal List: A Thriller",
-                BookConnectId = BookConnectId.Adventure
-            });
+        context.Books.Add(new Book
+        {
+            Title = "The Terminal List: A Thriller",
+            BookConnectId = BookConnectId.Adventure
+        });
 
-            context.Books.Add(new Book
-            {
-                Title = "Space Travel: Impossible to Reality",
-                BookConnectId = BookConnectId.SpaceTravel
-            });
+        context.Books.Add(new Book
+        {
+            Title = "Space Travel: Impossible to Reality",
+            BookConnectId = BookConnectId.SpaceTravel
+        });
 
-            context.Books.Add(new Book
-            {
-                Title = "Galaxies: Birth and Destiny of Our Universe",
-                BookConnectId = BookConnectId.SpaceTravel
-            });
+        context.Books.Add(new Book
+        {
+            Title = "Galaxies: Birth and Destiny of Our Universe",
+            BookConnectId = BookConnectId.SpaceTravel
+        });
 
-            context.Books.Add(new Book
-            {
-                Title = "100 cars that changed the world",
-                BookConnectId = BookConnectId.Automobile
-            });
+        context.Books.Add(new Book
+        {
+            Title = "100 cars that changed the world",
+            BookConnectId = BookConnectId.Automobile
+        });
 
-            context.Books.Add(new Book
-            {
-                Title = "Classic cars",
-                BookConnectId = BookConnectId.Automobile
-            });
+        context.Books.Add(new Book
+        {
+            Title = "Classic cars",
+            BookConnectId = BookConnectId.Automobile
+        });
 
+        context.Books.Add(new Book
+        {
+            Title = "Built for Speed: The World's Fastest Road Cars",
+            BookConnectId = BookConnectId.Automobile
+        });
 
-            context.Books.Add(new Book
-            {
-                Title = "Built for Speed: The World's Fastest Road Cars",
-                BookConnectId = BookConnectId.Automobile
-            });
+        context.SaveChanges();
 
-            context.SaveChanges();
-
-        }
-        
         return context
             .Books
             .Include(item => item.BookConnect)
             .ToList();
 
-            
- 
-
-
-
     }
-
-
-
 }
